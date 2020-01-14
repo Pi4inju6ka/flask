f9216713f0952f5ff520fe50ceb4f87f31bd2aeb
Pi4inju6ka <theboubfin@gmail.com>
2020-01-14 11:41:58 +0000

    add heroku files

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ def index():
 
 @app.route('/home')
 def home():
-  return"<2>"
+  return"<h1><a href='/about'>my home</a></h1>"
 
-@app.route('/abaut')
-def abaut():
-  return render_template('abaut.html')
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html',Phone=29647470)
+
+if __name__ =='__main__':
+  app.run(host="0.0.0.0",port=5232, threaded = True , debug = True) 
 
 
 
-
-
-app.run(host="0.0.0.0",port=8020)
-
-
-
-#heroku piregestret
